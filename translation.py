968289eeb79ef608be1678a1c52c871faff2bb9d
Wade Wilson <84837136+Sayandhridolickid@users.noreply.github.com>
2021-06-19 12:17:39 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -5,6 +5,7 @@ class Translation(object):
   START_TXT = """<b>Hai {}!!</b>
 <i>I'm Simple Auto file Forward Bot
 This Bot forward all files to One Public channel to Your Personal channel
+@Movievibezz
 More details /help</i>"""
   CAPTION = "`{}`\n\n" + str(Config.CAPTION)
   HELP_TXT = """<b>Follow These Steps!!</b>
@@ -22,7 +23,7 @@ More details /help</i>"""
   ABOUT_TXT = """<b><u>My Info</b></u>
 
 <b>Name :</b> <code>Auto Forward Bot</code>
-<b>Credit :</b> <code>Dark Angel</code>
+<b>Credit :</b> <code>@Ridolickid</code>
 <b>Language :</b> <code>Python3</code>
 <b>Lybrary :</b> <code>Pyrogram v1.2.9</code>
 <b>Server :</b> <code>Heroku</code>
